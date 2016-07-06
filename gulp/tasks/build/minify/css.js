5859e7ca71b3f5d56c22c17b30118857e6bf15c9
jfernandez89 <jfernandez.developer@gmail.com>
2016-07-06 10:48:19 +0200

    Merge css / inject

--- gulp/tasks/build/minify/css.js
+++ gulp/tasks/build/minify/css.js
@@ -1,4 +1,5 @@
 var plugins = require('gulp-load-plugins')({lazy: true});
+var merge = require('merge-stream');
 
 var args = require('yargs').argv;
 
@@ -13,13 +14,23 @@ var config = require(global.GULP_DIR + '/gulp.config');
 module.exports = {
   dep: ['styles:' + config.style.framework],
   fn: function (gulp, done) {
-    utils.log('***  Minifiying && hashing css ***');
 
-    return gulp.src(config.paths.css.dest + config.paths.css.fileName)
+    utils.log('***  Minifiying, hashing && injecting css  ***');
+
+    //Minify && hash
+    var css = gulp.src(config.paths.css.dest + config.paths.css.fileName)
       .pipe(plugins.if(args.verbose, plugins.bytediff.start()))
       .pipe(plugins.minifyCss({keepSpecialComments: 0}))
       .pipe(plugins.if(args.verbose, plugins.bytediff.stop()))
       .pipe((plugins.hashFilename({"format": "{name}.{hash}.min{ext}"})))
       .pipe(gulp.dest(config.paths.css.dest));
+
+    //Inject
+    var inject = gulp
+      .src(config.paths.html.index)
+      .pipe(plugins.inject(gulp.src(config.paths.css.dest + '*min.css', {read: false}), {relative: true}))
+      .pipe(gulp.dest('./'));
+
+    return merge(css, inject);
   }
 };
