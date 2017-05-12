c99b220c70c19319c6cc008b204616a1b4aff220
Alejandro Díaz Torres <aledt84@gmail.com>
2017-05-12 09:22:58 +0200

    SassLint configuration and proxy fix (#4)
    * Add new dependency 'extend': https://github.com/justmoon/node-extend
    * Add 'tunnel-agent' dependency > 0.4.1 to avoid errors using proxies
    * Now you can generate a sassLint report using config.sassLint in gulp.config.js file

--- gulp/tasks/analyze/sass.js
+++ gulp/tasks/analyze/sass.js
@@ -1,6 +1,8 @@
 var plugins = require('gulp-load-plugins')({lazy: true});
 
 var args = require('yargs').argv;
+var fs = require('fs');
+var extend = require('extend');
 
 var utils = require(global.GULP_DIR + '/utils');
 var config = require(global.CONFIG_PATH || global.GULP_DIR + '/gulp.config');
@@ -16,11 +18,21 @@ module.exports = {
   fn: function (gulp, done) {
     utils.log('***  Performing sass lint analysis ***');
 
+    // read options from configuration
+    var file = null;
+    var opts = {};
+    if(config.sassLint){
+      if(config.sassLint['output-file']){
+        file = fs.createWriteStream(config.sassLint['output-file']);
+      }
+      extend(opts, config.sassLint);
+    }
+
     return gulp
       .src(config.paths[config.style.framework].dev)
       .pipe(plugins.if(!args.exhaustive, plugins.cached('sass-lint')))
-      .pipe(plugins.sassLint())
-      .pipe(plugins.sassLint.format())
+      .pipe(plugins.sassLint(opts))
+      .pipe(plugins.sassLint.format(file))
       .pipe(plugins.sassLint.failOnError());
   }
 };
